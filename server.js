2e640868d2e470c1812dd6a2318630a9c4e85703
Adam <adam_ali_96@hotmail.co.uk>
2016-10-19 13:14:52 +0100

    testing

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ mongoose.connect('mongodb://localhost:27017/booking', function(err, db) {
     }
 });
 app.use(express.static(__dirname+'/public'));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
