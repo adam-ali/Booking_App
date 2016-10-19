68846f35caf5b12a1241c284c8141b625a53512c
Adam <adam_ali_96@hotmail.co.uk>
2016-10-19 13:58:23 +0100

    testing

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use(express.static(__dirname+'/public'));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+
     next();
 });
 app.use(bodyParser.urlencoded({ extended: false }));
