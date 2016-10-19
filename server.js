fe0d8a5ab18e70fb8d5f1106e1549b4fb8d8ef67
Adam <adam_ali_96@hotmail.co.uk>
2016-10-19 13:59:25 +0100

    testing

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static(__dirname+'/public'));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
 
     next();
 });
