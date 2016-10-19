9781f39d3006602174caf3e4755aedbfb866391a
Adam <adam_ali_96@hotmail.co.uk>
2016-10-19 14:10:57 +0100

    testing

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,13 @@ module.exports = {
                 ]
             },
             {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
+            },
+            {
                 test: /\.css$/,
                 loader: "style-loader!css-loader"
             },
